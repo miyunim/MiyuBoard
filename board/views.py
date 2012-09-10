669eff3994729dfb10951438208dbb6f3e1c7185
miyu <miyu@miyunim.cafe24.com>
2012-09-10 20:41:10 +0900

    file update

--- board/views.py
+++ board/views.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse, HttpResponseRedirect
-from django.template import Context
+from django.template import Context, RequestContext
 from django.template.loader import get_template
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from django.shortcuts import render_to_response
+from board.forms import *
+
 
 def main_page(request):
 	template = get_template('main_page.html')
@@ -15,3 +19,22 @@ def main_page(request):
 def logout_page(request):
 	logout(request)
 	return HttpResponseRedirect('/')
+
+def register_page(request):
+    print request.method
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username = form.cleaned_data['username'],
+                password = form.cleaned_data['password1'],
+                email = form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/register/success/')
+    else:
+        form = RegistrationForm()
+
+    variables = RequestContext(request, {
+        'form': form
+    })
+    return render_to_response('registration/register.html', variables)
